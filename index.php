aa7239234f264865720f5fe09e31a9ca3fa8fb83
sarsdeyoung <Sarah@sarah-deyoungs-imac.local>
2014-02-06 20:12:57 -0600

    Added header

--- index.php
+++ index.php
@@ -1,23 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title><?php bloginfo('name'); ?></title>
-	<script src="http://use.edgefonts.net/josefin-slab.js"></script>
-	<script src="http://use.edgefonts.net/merriweather.js"></script>
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>"/>
-</head>
-	<body>
-		<header>
-			<h1><img src="<?php bloginfo('stylesheet_directory'); ?>/images/orange.jpg" />
-            <?php bloginfo('name'); ?></h1>
-			<nav>
-				<ul>
-					<li>Home</li>
-					<li>WordPress</li>
-				</ul>
-			</nav>
-		</header>
-		<section>
 <?php get_header(); ?>
 <div class="clear">
 		<section class="main">
@@ -38,13 +18,7 @@
             <?php endif; ?>
             
 		</section>
-		<footer>
-			<h1>Contact Us</h1>
-			<p>Pressed and Squeezed</p>
-			<p>2525 Orange Way, Orchard, CA 91104</p>
-			<p>Phone: (800) 424-3232</p>
-		</footer>
-	</body>
-</html>        
+        
         <?php get_sidebar(); ?>
-  </div><?php get_footer(); ?>
+  </div>
+<?php get_footer(); ?>
